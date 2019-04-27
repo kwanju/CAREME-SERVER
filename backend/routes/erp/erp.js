2067c1e57c8004e5517b64bcda55c50fde33e0cd
ahn7253 <pyungryulahn1009@gmail.com>
2019-04-27 20:36:56 +0900

    update ERP

--- backend/routes/erp/erp.js
+++ backend/routes/erp/erp.js
@@ -2,9 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 var mainRouter = require('./main/main');
+var animalRouter = require('./animal/animal');
+var volunteerRouter = require('./volunteer/volunteer');
+var volunteerRouter = require('./mshelter/mshelter');
 
 
 router.use('/main',mainRouter);
+router.use('/animal', animalRouter);
+router.use('/volunteer', volunteerRouter);
+router.use('/mshelter', mshelterRouter);
 
 
 module.exports = router;
--- backend/routes/erp/main/page.js
+++ backend/routes/erp/main/page.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 router.get("/main", function(_req,_res){
-  _res.render('layout/main2', {  body:"../erp/erp",  csession:_req.csession});
+  //_res.render('layout/main2', {  body:"../erp/erp",  csession:_req.csession});
+  _res.render('layout/main2', {body:"../erp/erp",  csession:_req.csession});
 
 });
 

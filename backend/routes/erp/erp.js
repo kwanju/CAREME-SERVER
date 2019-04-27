0e90b73272730766ee8d4102308a29f273cd9863
kwanju <rhkswn123@ajou.ac.kr>
2019-04-28 03:20:20 +0900

    notify test

--- backend/routes/erp/erp.js
+++ backend/routes/erp/erp.js
@@ -5,13 +5,13 @@ var mainRouter = require('./main/main');
 var animalRouter = require('./animal/animal');
 var volunteerRouter = require('./volunteer/volunteer');
 var mshelterRouter = require('./mshelter/mshelter');
-var notifyRouter = require('./notifyTest/notify');
+var notifyRouter = require('./notifytest/notify');
 
 
 router.use('/main',mainRouter);
 router.use('/animal', animalRouter);
 router.use('/volunteer', volunteerRouter);
 router.use('/mshelter', mshelterRouter);
-router.use('/notify', notifyRouter);
+router.use('/notifytest', notifyRouter);
 
 module.exports = router;
--- backend/routes/erp/notifytest/page.js
+++ backend/routes/erp/notifytest/page.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-router.get("/erp/notifytest", function(_req,_res){
+router.get("/notifytest", function(_req,_res){
   _res.render('layout/main2', { body:"../erp/notifytest",  csession:_req.csession});
 
 });

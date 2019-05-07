1f0c292c061dd56a32e8e7ca2826fcbeffcb8fd0
pphhss <gustjs2080@naver.com>
2019-05-07 23:11:02 +0900

    버그 수정

--- backend/routes/erp/erp.js
+++ backend/routes/erp/erp.js
@@ -6,7 +6,6 @@ var animalRouter = require('./animal/animal');
 var volunteerRouter = require('./volunteer/volunteer');
 var mshelterRouter = require('./mshelter/mshelter');
 var notifyRouter = require('./notifytest/notify');
-var scheduleRouter = require('./schedule/schedule');
 
 router.use('/main',mainRouter);
 router.use('/animal', animalRouter);

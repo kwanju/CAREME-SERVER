45638f22704f91b46d4e1f49b1417e4630828c77
kwanju <rhkswn123@ajou.ac.kr>
2019-05-07 14:36:11 +0900

    알림 ejs->js,카카오맵 api 받아오기 1차

--- backend/app.js
+++ backend/app.js
@@ -11,6 +11,7 @@ var indexRouter = require('./routes/index');
 var shelterRouter = require('./routes/shelter/shelter');
 var erpRouter = require('./routes/erp/erp');
 var androidRouter = require('./routes/android/android');
+var mapRouter = require('./routes/map/mapTest');
 
 var app = express();
 
@@ -38,6 +39,7 @@ app.use('/', indexRouter);
 app.use('/user', shelterRouter);
 app.use('/erp',erpRouter);
 app.use('/android',androidRouter);
+app.use('/mapTest', mapRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

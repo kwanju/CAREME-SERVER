3f6f4c21b2020eb8c56b18da533629448cbb8122
pphhss <gustjs2080@naver.com>
2019-05-15 18:21:57 +0900

    발견했어요 가져오는 부분 버그 수정

--- backend/routes/erp/discover/json.js
+++ backend/routes/erp/discover/json.js
@@ -1,14 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var insertIdxIntoBody = require('../../../utils/erp/insertIdxIntoBody');
 
-router.post('/getDiscoverRequestWaiting', function (_req, _res) {
+router.post('/getDiscoverRequestWaiting', insertIdxIntoBody, function (_req, _res) {
     var discover = require('../../../model/erp/discover');
     discover.getDiscoverRequestWaiting(_req.body, function (_result) {
         _res.send(_result);
     });
 });
 
-router.post('/getDiscoverRequestRecord', function (_req, _res) {
+router.post('/getDiscoverRequestRecord', insertIdxIntoBody, function (_req, _res) {
     var discover = require('../../../model/erp/discover');
     discover.getDiscoverRequestRecord(_req.body, function (_result) {
         _res.send(_result);

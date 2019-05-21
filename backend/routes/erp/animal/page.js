a425178bdd3a9423dc5e8d8f8a8f7e0d940713a8
ahn7253 <pyungryulahn1009@gmail.com>
2019-05-21 15:47:19 +0900

    add table

--- backend/routes/erp/animal/page.js
+++ backend/routes/erp/animal/page.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get("/animal", function(_req,_res){
-  _res.render('layout/main2', { body:"../erp/animal",head:"../erp/head/indexHead",javascript:"../erp/javascript/indexJavascript",  csession:_req.csession});
+  _res.render('layout/main2', { body:"../erp/animal",head:"../erp/head/tableHead",javascript:"../erp/javascript/tableJavascript",  csession:_req.csession});
 });
 
 router.get('/imagetest',function(_req,_res){

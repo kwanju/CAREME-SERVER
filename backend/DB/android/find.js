334d1b577a0a59a0d6dc2e016d89c9acffc18501
pphhss <gustjs2080@naver.com>
2019-05-22 22:04:46 +0900

    찾아요 등록 날짜시간 추가

--- backend/DB/android/find.js
+++ backend/DB/android/find.js
@@ -1,12 +1,16 @@
 var poolAdapter = require('../poolAdapter');
 
 exports.registerFind = function (_data, _callback) {
+    var data = _data;
     var insert = "INSERT INTO find SET ?"
     /* var dataName = "(user_idx, lost_datetime, lost_spot, description, species_code, " +
          "animal_sex, url_picture, register_datetime, longitude, latitude) "
      var values = "VALEUS(?,?,?,?,?" +
          "?,?,?,?,?)";
      */
+
+    var registerDate = require('../../utils/date')();
+    data.register_datetime = registerDate;
     poolAdapter.execute(insert, _data, function (_results) {
         _callback(_results.insertId);
     });

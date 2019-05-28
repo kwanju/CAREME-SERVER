2c3a3ef57ec18288ba99a404edfc9019806dd8be
kwanju <rhkswn123@ajou.ac.kr>
2019-05-28 15:57:36 +0900

    입양페이지에서 보호소 검색 & 유기견 검색

--- backend/routes/erp/adopt/json.js
+++ backend/routes/erp/adopt/json.js
@@ -1,26 +1,5 @@
 var express = require('express');
 var router = express.Router();
 
-// var insertIdxIntoBody = require('../../../utils/erp/insertIdxIntoBody');
-
-// router.post('/getAnimalList', insertIdxIntoBody, function (_req, _res) {
-//     var animal = require('../../../model/erp/animal');
-//     animal.getAnimalList(_req.body, function (_result) {
-//         _res.send(_result);
-//     });
-// });
-
-// router.post('/getAnimalListPage', insertIdxIntoBody, function (_req, _res) {
-//     var animal = require('../../../model/erp/animal');
-//     animal.getAnimalListPage(_req.body, function (_result) {
-//         _res.send(_result);
-//     });
-// });
-
-// router.post('/getAnimal', function (_req, _res) {
-//     var animal = require('../../../model/erp/animal');
-//     animal.getAnimal(_req.body, function (_result) {
-//         _res.send(_result);
-//     });
-// });
+// ;
 module.exports = router;
--- backend/routes/erp/search/json.js
+++ backend/routes/erp/search/json.js
@@ -1,13 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-router.post('/searchAnimalByName', function (_req, _res) {
+router.post('/searchAnimal', function (_req, _res) {
     var animal = require('../../../model/erp/search');
-    //if(_req.session.user.idx) {
-    //    _req.body.idx = _req.session.user.idx;
-    //}
-    _req.body.idx = 1; //for test
-    animal.searchAnimalByName(_req.body, function (_result) {
+    //_req.body -> shelter idx, type: 검색할 속성, text: 검색할 문자열
+    animal.searchAnimalByName(_req.body, function (_result) { //byName 이지만 name외에도 검색 가능(이름수정을 아직 안했음)
+        _res.send(_result);
+    });
+});
+
+router.post('/searchShelterByName', function (_req, _res) {
+    var shelter = require('../../../model/erp/search');
+
+    shelter.searchShelterByName(_req.body, function (_result) {
         _res.send(_result);
     });
 });

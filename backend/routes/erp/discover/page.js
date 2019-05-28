40ce4a10fb3c27e2e3f4fdae4cb4d78d0191e13f
ahn7253 <pyungryulahn1009@gmail.com>
2019-05-28 17:48:43 +0900

    modify head and javascript files

--- backend/routes/erp/discover/page.js
+++ backend/routes/erp/discover/page.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 
 router.get('/', function (_req, _res) {
-    _res.render('layout/main2', { body: "../erp/discoverRequest",head:"../erp/head/tableHead",javascript:"../erp/javascript/tableJavascript", csession: _req.csession });
+    _res.render('layout/main2', { body: "../erp/discoverRequest",head:"../erp/head/discoverRequestHead",javascript:"../erp/javascript/discoverRequestJavascript", csession: _req.csession });
 });
 
 module.exports = router;
--- backend/routes/erp/volunteer/page.js
+++ backend/routes/erp/volunteer/page.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get("/volunteer", function(_req,_res){
-  _res.render('layout/main2', { body:"../erp/volunteer",head:"../erp/head/tableHead",javascript:"../erp/javascript/tableJavascript",  csession:_req.csession});
+  _res.render('layout/main2', { body:"../erp/volunteer",head:"../erp/head/volunteerHead",javascript:"../erp/javascript/volunteerJavascript",  csession:_req.csession});
 });
 
 module.exports = router;

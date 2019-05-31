ccc323f8b0a223d3218ed9e2926d7926d0360984
pphhss <gustjs2080@naver.com>
2019-06-01 00:50:34 +0900

    [안드로이드] 봉사 기록에 animal_idx 추가

--- backend/DB/android/schedule.js
+++ backend/DB/android/schedule.js
@@ -2,7 +2,7 @@ var poolAdapter = require('../poolAdapter');
 
 exports.getUserSchedule = function (_data, _callback) {
     var whereSchedule = "WHERE user_idx=?";
-    var sql = "SELECT sch.date, sch.permit, ani.name AS animalName, slt.name AS shelterName, ani.url_picture FROM " +
+    var sql = "SELECT sch.date, sch.permit, ani.name AS animalName, slt.name AS shelterName, ani.url_picture, ani.idx AS animal_idx FROM " +
         "(SELECT * FROM schedule " + whereSchedule + ") AS sch " +
         "INNER JOIN animal AS ani INNER JOIN shelter AS slt ";
     var on = "ON sch.animal_idx=ani.idx AND slt.idx=ani.shelter_idx ";

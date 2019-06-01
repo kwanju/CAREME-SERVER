a7f969c5f272a2d3173a0b66e60601bed91a9857
ahn7253 <pyungryulahn1009@gmail.com>
2019-06-02 03:27:10 +0900

    update adopt registration form

---
+++ backend/routes/adoptDetail/json.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+// router.post('/userLogin', function (_req, _res) {
+//     var adopt = require('../../model/erp/adopt');
+//     adopt.userLogin(_req.body, function (_result) {
+//         _res.send(_result);
+//     });
+// });
+
+module.exports = router;

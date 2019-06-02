370f90d19d0c46836d5d47cbcca318efbe4d59ba
pphhss <gustjs2080@naver.com>
2019-06-03 00:12:13 +0900

    입양 신청 디자인 수정

--- backend/routes/adoptDetail/page.js
+++ backend/routes/adoptDetail/page.js
@@ -3,8 +3,7 @@ var router = express.Router();
 
 router.get("/adoptDetail", function(_req,_res){
   //_res.render('layout/main2', {  body:"../erp/erp",  csession:_req.csession});
-  _res.render('layout/main', {body:"../erp/adoptDetail", head:"../erp/head/indexHead",javascript:"../erp/javascript/indexJavascript",  csession:_req.csession});
-
+  _res.render('erp/adoptDetail', {csession:_req.csession});
 });
 
 module.exports = router;

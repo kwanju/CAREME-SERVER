f78d19fe2fb21809bee5b86eb5638e4799f89b09
pphhss <gustjs2080@naver.com>
2019-06-03 20:20:37 +0900

    채팅 디비 스키마 변경 및 관련 모듈 적용

--- backend/routes/erp/chat/json.js
+++ backend/routes/erp/chat/json.js
@@ -2,5 +2,7 @@ var express = require('express');
 var router = express.Router();
 var insertIdxIntoBody = require('../../../utils/erp/insertIdxIntoBody');
 
-
+exports.getChatList = function () {
+    
+}
 module.exports = router;
